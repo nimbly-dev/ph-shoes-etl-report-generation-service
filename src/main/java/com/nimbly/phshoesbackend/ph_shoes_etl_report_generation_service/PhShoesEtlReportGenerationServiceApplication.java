ddb1dd966399c751cfcb0f09c731ade7c7849fc5
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-08-24 21:26:33 +0800

    PHSHOE-1: Initialize Microservice Project for ETL PH Shoes Report

---
+++ src/main/java/com/nimbly/phshoesbackend/ph_shoes_etl_report_generation_service/PhShoesEtlReportGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nimbly.phshoesbackend.ph_shoes_etl_report_generation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhShoesEtlReportGenerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhShoesEtlReportGenerationServiceApplication.class, args);
+	}
+
+}
